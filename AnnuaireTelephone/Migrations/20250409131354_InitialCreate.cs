3ec8e5d340cb0e7df5b01d62a16d601aed917266
Darnel Eteno <josephdarneleteno59@gmail.com>
2025-04-09 13:27:21 +0000

    Implementation de migrations et autres.

---
+++ AnnuaireTelephone/Migrations/20250409131354_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AnnuaireTelephone.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Matricule = table.Column<string>(type: "text", nullable: false),
+                    Nom = table.Column<string>(type: "text", nullable: false),
+                    Prenom = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telephones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Numero = table.Column<string>(type: "text", nullable: false),
+                    ClientId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telephones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Telephones_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_Matricule",
+                table: "Clients",
+                column: "Matricule",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telephones_ClientId",
+                table: "Telephones",
+                column: "ClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Telephones");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
